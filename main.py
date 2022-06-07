3b468bed4b6f86a6cf2c05ba163711875146fc9d
EugenijusBu <eugenijus.bulvydas@ku.lt>
2022-06-07 22:16:38 +0300

    first deploy

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    sentence: str
+    letters: str
+
+@app.post("/modify")
+def modify(item: Item):
+    s = item.sentence.replace(item.letters,"")
+    return{s}
